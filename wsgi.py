0fe8d24d398249cf787e1035be9e260548ff1c4d
Mushfiquehk <mushfiquehasankhan@gmail.com>
2025-01-02 20:13:45 -0600

    fixed broken link to home

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ path = '/home/Mushfique/read-my-mind'
 if path not in sys.path:
    sys.path.insert(0, path)
 
-from flask_app import app as application
+from application import app as application
 
 
 if __name__ == "__main__":
