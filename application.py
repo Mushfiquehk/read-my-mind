3b3241c8ff7db1d31e1b0f22c250c664e31b9167
Mushfiquehk <mushfiquehasankhan@gmail.com>
2025-01-04 23:40:23 -0600

    added template and static dir to app

--- application.py
+++ application.py
@@ -2,14 +2,31 @@
 
 """
 
-import sys
-from app import create_app
 
-path = "/home/Mushfique/read-my-mind"
-if path not in sys.path:
-    sys.path.insert(0, path)
+from flask import Flask
+from app.config import Config
+from app.views.main import main
+from app.views.spotify import spotify
+from app.views.wrapped import wrapped
 
-app = create_app()
+
+def create_app(config_class=Config):
+    """Create and configure an instance of the Flask application."""
+    app = Flask(__name__,
+                template_folder="app/templates",
+                static_folder="app/static")
+
+    Config.init_app(app)
+    app.config.from_object(config_class)
+
+    # Register blueprints
+    app.register_blueprint(main)
+    app.register_blueprint(spotify, url_prefix="/spotify")
+    app.register_blueprint(wrapped, url_prefix="/wrapped")
+
+    return app
+
+application = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    application.run(debug=True)
